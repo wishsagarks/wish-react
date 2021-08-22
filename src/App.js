0dfa544027a23839dc5ccaa7d9acea7d9d4c14f2
SAGAR SATAPATHY <wishsagarks@gmail.com>
2021-08-23 02:38:26 +0530

    darkMode
    DarkMode intialised!

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+<<<<<<< Updated upstream
 
 function App() {
   return (
@@ -85,6 +85,26 @@ function App() {
 </div>
 </div>
 
+=======
+import Navbar from './components/Navbar'
+// import Slider from './components/Slider';
+// import Textform from './components/Textform';
+import Aboutme from './components/Aboutme';
+function App() { 
+  return (
+    <>
+    
+{/* importing navbar */}
+<Navbar title="Home"/>
+{/* sllider */}
+{/* <Slider /> */}
+
+<br>
+</br>
+{/* text form */}
+{/* <Textform name="Enter your Name:" area="Enter your text here:"/> */}
+    <Aboutme/>
+>>>>>>> Stashed changes
 
     </>
   );
