08198cdbc1459feb33241aa4d808e10d4f7c4969
SAGAR SATAPATHY <wishsagarks@gmail.com>
2021-08-23 19:58:38 +0530

    update
    updated

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -13,22 +13,28 @@ export default function slider() {
     <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
   <div className="carousel-inner">
+
+
+
+
+
+
     <div className="carousel-item active" data-bs-interval="5000">
-      <img src="1.jpg" height="300px" width="auto" className="d-block w-100" alt="1st slide"/>
+      <img src="https://i.ibb.co/KVH6Mwr/1.jpg" height="300px" width="auto" className="d-block w-100" alt="1st slide"/>
       <div className="carousel-caption d-none d-md-block">
         <h5>Wish is building</h5>
         <p>It’s gonna be legend-... wait for it…DAIRY!</p>
       </div>
     </div>
     <div className="carousel-item" data-bs-interval="2000">
-      <img src="2.jpg" height="300px" width="auto" className="d-block w-100" alt="2nd slide"/>
+      <img src="https://i.ibb.co/Fht1ggj/2.jpg" height="300px" width="auto" className="d-block w-100" alt="2nd slide"/>
       <div className="carousel-caption d-none d-md-block">
       <h5>Wish is building 2nd slide</h5>
       <p>It’s gonna be legend-... wait for it…DAIRY!wait for it na</p>
       </div>
     </div>
     <div className="carousel-item">
-      <img src="3.jpg" height="300px" width="auto" className="d-block w-100" alt="3rd slide"/>
+      <img src="https://i.ibb.co/t3VNG94/3.jpg" height="300px" width="auto" className="d-block w-100" alt="3rd slide"/>
       <div className="carousel-caption d-none d-md-block">
       <h5>Wish is building 3rd slide</h5>
       <p>It’s gonna be legend-... wait for it…DAIRY!coming soon1</p>
