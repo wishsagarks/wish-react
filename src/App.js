8adc2e255c9e1fcbd238f6540dfd70e2dae489e5
SAGAR SATAPATHY <wishsagarks@gmail.com>
2021-08-23 19:53:27 +0530

    swithced
    changed

--- src/App.js
+++ src/App.js
@@ -3,21 +3,36 @@ import './App.css';
 import Navbar from './components/Navbar'
 import Slider from './components/Slider';
 import Textform from './components/Textform';
+import React, { useState } from 'react'
+
 // import Aboutme from './components/Aboutme';
-function App() { 
+function App() {
+  const [mode, setmode] = useState('light');
+  const toggleMode = () => {
+    if (mode === 'light') 
+    { 
+      setmode('dark'); 
+    }
+    else 
+    {
+      setmode('light');
+
+    }
+  }
+
   return (
     <>
-    
-{/* importing navbar */}
-<Navbar title="Home"/>
-{/* sllider */}
-<Slider />
 
-<br>
-</br>
-{/* text form */}
-<Textform name="Enter your Name:" area="Enter your text here:"/>
-    {/* <Aboutme/> */}
+      {/* importing navbar */}
+      <Navbar title="Home" mode={mode} toggleMode={toggleMode} />
+      {/* sllider */}
+      <Slider />
+
+      <br>
+      </br>
+      {/* text form */}
+      <Textform name="Enter your Name:" area="Enter your text here:" />
+      {/* <Aboutme/> */}
 
     </>
   );
