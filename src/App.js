506df27e868e7cc8049e576923943392cd5cae3f
SAGAR SATAPATHY <wishsagarks@gmail.com>
2021-09-18 18:46:44 +0530

    alert

--- src/App.js
+++ src/App.js
@@ -3,25 +3,38 @@ import './App.css';
 import Navbar from './components/Navbar'
 import Slider from './components/Slider';
 import Textform from './components/Textform';
+import Alert from './components/Alert';
 import React, { useState } from 'react'
 
 // import Aboutme from './components/Aboutme';
 function App() {
   const [mode, setmode] = useState('dark');
-  
+  const [alert, setalert] = useState(null);
 
+  const showalert = (message, type) => {
+    setalert({
+      msg: message,
+      type: type
+    })
+    setTimeout (() => {
+      setalert(null);
+
+    }, 3000);
+  }
 
   const toggleMode = () => {
-    if (mode === 'light') 
-    { 
-      setmode('dark'); 
-      document.body.style.backgroundColor="black";
+    if (mode === 'light') {
+      setmode('dark');
+      document.body.style.backgroundColor = "black";
+      showalert("set to light", "success");
+
+
 
     }
-    else 
-    {
-      setmode('light'); 
-      document.body.style.backgroundColor="#2497d1";
+    else {
+      setmode('light');
+      document.body.style.backgroundColor = "#2497d1";
+      showalert("set to dark", "success");
     }
   }
 
@@ -29,7 +42,8 @@ function App() {
     <>
 
       {/* importing navbar */}
-      <Navbar title="Home" mode={mode} toggleMode={toggleMode}  />
+      <Navbar title="Home" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
       {/* sllider */}
       <Slider />
 
